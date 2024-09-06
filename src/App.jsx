08299184d24f42bb145f0797241bdf612f3931fb
jourdannf <jourdann.fraser@gmail.com>
2024-09-06 14:29:37 -0400

    Users can no longer guess if they've used up their 3 tries and can't interact with select button once they've already correctly guessed the member; re-loads renders with the correct guesses you've already made

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,105 @@ import './App.css'
 
 function App() {
 
-  const [date, setDate] = useState(new Date());
+  const [dateNow, setDate] = useState(processDate(new Date()));
   const [pictures, setPictures] = useState([]);
   const [answer, setAnswers] = useState([]);
-  const [guess, setGuess] = useState([]);
+  const [guess, setGuess] = useState(["","","","",""]);
+  const [tries, setTries] = useState(3);
+  const [correctGuess, setCorrectGuess] = useState(localStorage.getItem("guessState").split(",").map(e => Number(e)))
 
   useEffect(() => {
 
     (async () => {
-      const result = await axios.get(`http://localhost:3001/v1/puzzle/${date.toISOString().split('T')[0]}`)
+      const result = await axios.get(`http://localhost:3001/v1/puzzle/${new Date(dateNow).toISOString().split('T')[0]}`)
       const puzzle = result.data;
 
       setPictures(puzzle.answer.pictures);
       setAnswers(puzzle.answer.names);
     })();
     
-  }, [date])
+  }, [])
 
-  function onSubmit (e) {
+  function submitGuess (e) {
     e.preventDefault();
 
-    if ((guess[0] == answer[0]) && (guess[1] == answer[1]) && (guess[2] == answer[2]) && (guess[3] == answer[3]) && (guess[4] == answer[4])) {
-      console.log("You're correct!")
+    if (localStorage.getItem("triesComplete") == "true"){
+      console.log("You can no longer guess :(");
+      return;
     }
 
-    console.log("You're wrong :(")
+    const finalGuess = guess.map((e, i) => {
+      if (localStorage.guessState.split(",")[i] == "1"){
+        return answer[i];
+      }
+
+      return e;
+    });
+
+    if (finalGuess.includes("")) {
+      console.log("You haven't answered everything yet");
+      return;
+    }
+
+    if (localStorage.getItem("triesLeft") == 1){
+      localStorage.setItem("triesComplete", true);
+    }
+
+    localStorage.setItem("guess", guess);
+    localStorage.setItem("triesLeft", tries-1);
+
+    setTries(tries - 1);
+
+    if ((finalGuess[0] == answer[0]) && (finalGuess[1] == answer[1]) && (finalGuess[2] == answer[2]) && (finalGuess[3] == answer[3]) && (finalGuess[4] == answer[4])) {
+      console.log("You're correct!");
+      setCorrectGuess([1,1,1,1,1]);
+      return;
+    }else {
+      console.log("You're wrong :(");
+      const newGuess = correctGuess.map((elem, i) => {
+        if (finalGuess[i] == answer[i]){
+          return 1;
+        }
+  
+        return 0;
+      });
+      localStorage.setItem("guessState", newGuess);
+      setCorrectGuess(newGuess);
+    }
+
   }
 
+  function processDate(date) {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  }
+  
+  if (localStorage.getItem("date") != dateNow) {
+    localStorage.setItem("date", dateNow);
+    localStorage.setItem("triesComplete", false);
+    localStorage.setItem("triesLeft", 3);
+    localStorage.setItem("guess", ["","","","","",""]);
+    localStorage.setItem("guessState", [false, false, false, false, false])
+    return true;
+  }
 
-  return (
-    <>
-      <h1>IRRIS Guessing Game</h1>
+  
 
-      {pictures.map((picLink, i) => {
-        return <Photo key={i} link={picLink} num={i+1} guess={guess} />
-      })}
-      <Photo />
-      <br />
-      <input type='submit' />
-    </>
-    
-  )
+  if (localStorage.getItem("triesComplete") == "false"){
+    return (
+      <>
+        <h1>IRRIS Guessing Game</h1>
+  
+        {pictures.map((picLink, i) => {
+          return <Photo key={i} link={picLink} num={i} guess={guess} updateGuess={setGuess} answerCheck={correctGuess} answer={answer}/>
+        })}
+        
+        <input type='submit' onClick={submitGuess} />
+      </>
+      
+    )
+  }
+
+  
 }
 
 export default App
--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,18 +1,53 @@
-export default function Photo ({num, link}) {
+export default function Photo ({num, link, guess, updateGuess, answerCheck, answer}) {
+
+    function selectMember (e) {
+        
+        if (e.target.value == "default") {
+            updateGuess(guess.map((elem, i) => {
+                if (i == num){
+                    return elem = "";
+                }
+    
+                return elem;
+            }));
+
+            return;
+        }
+
+        updateGuess(guess.map((elem, i) => {
+            if (i == num){
+                return elem = e.target.value;
+            }
+
+            return elem;
+        }));
+    }
+
+    function titleCase(string){
+        if (string == "i.l"){
+            return "I.L";
+        }
+
+        return string[0].toUpperCase() + string.slice(1).toLowerCase();
+    }
 
     return (
         <div>
         
-            <img src={link} alt={`Cropped Photo of Member ${num}`} />
+            <img src={link} alt={`Cropped Photo of Member ${num + 1}`} />
             <br />
-            <select> 
-                <option value="Please select a member"> Please select a member</option>
-                <option value="i.l">I.L</option>
-                <option value="liv">Liv</option>
-                <option value="yunseul">Yunseul</option>
-                <option value="nina">Nina</option>
-                <option value="nobody">Nobody</option>
-            </select>
+
+            {answerCheck[num] 
+                ? <h4> {titleCase(answer[num])} </h4> 
+                : <select onChange={selectMember}> 
+                    <option value="default"> Please select a member</option>
+                    <option value="i.l">I.L</option>
+                    <option value="liv">Liv</option>
+                    <option value="yunseul">Yunseul</option>
+                    <option value="nina">Nina</option>
+                    <option value="nobody">Nobody</option>
+                </select>}
+            
            
         </div>
     )
