7dc93ecae8637cc0d2cbc0a3f5426b2799d35d6a
jourdannf <jourdann.fraser@gmail.com>
2024-09-17 18:17:48 -0400

    Fixed intial useState values

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
   const [answer, setAnswers] = useState([]);
   const [guess, setGuess] = useState(["","","","",""]);
   const [tries, setTries] = useState(localStorage.triesLeft);
-  const [correctGuess, setCorrectGuess] = useState(JSON.parse(localStorage.getItem("guessState")));
-  const [freqArr, setFreqArr] = useState(JSON.parse(localStorage.freqArr));
+  const [correctGuess, setCorrectGuess] = useState(localStorage.guessState ? JSON.parse(localStorage.getItem("guessState")) : []);
+  const [freqArr, setFreqArr] = useState(localStorage.freqArr ? JSON.parse(localStorage.freqArr) : []);
 
   useEffect(() => {
 
@@ -116,16 +116,16 @@ function App() {
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
   }
   
-  if (localStorage.getItem("date") != dateNow) {
-    localStorage.setItem("date", dateNow);
-    localStorage.setItem("triesComplete", false);
-    localStorage.setItem("triesLeft", 3);
-    localStorage.setItem("guess", JSON.stringify(["","","","","",""]));
-    localStorage.setItem("guessState", JSON.stringify([0,0,0,0,0]));
-    localStorage.setItem("solved", false);
-    localStorage.setItem("freqArr", JSON.stringify([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]));
-    return true;
-  }
+  // if (localStorage.getItem("date") != dateNow) {
+  //   localStorage.setItem("date", dateNow);
+  //   localStorage.setItem("triesComplete", false);
+  //   localStorage.setItem("triesLeft", 3);
+  //   localStorage.setItem("guess", JSON.stringify(["","","","","",""]));
+  //   localStorage.setItem("guessState", JSON.stringify([0,0,0,0,0]));
+  //   localStorage.setItem("solved", false);
+  //   localStorage.setItem("freqArr", JSON.stringify([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]));
+  //   return true;
+  // }
 
   
 
