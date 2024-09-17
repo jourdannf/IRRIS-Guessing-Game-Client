90aa49ee76a22fc070579928d5ff6e253bae2f95
jourdannf <jourdann.fraser@gmail.com>
2024-09-17 18:55:45 -0400

    Made a quick demo is over page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Photo from './components/Photo'
 import PhotoContainer from './components/PhotoContainer'
 import axios from 'axios'
 import './App.css'
+import ComingSoonPage from './pages/ComingSoonPage'
+import { data } from 'autoprefixer'
 
 function App() {
 
@@ -15,15 +17,21 @@ function App() {
   const [tries, setTries] = useState(localStorage.triesLeft);
   const [correctGuess, setCorrectGuess] = useState(localStorage.guessState ? JSON.parse(localStorage.getItem("guessState")) : []);
   const [freqArr, setFreqArr] = useState(localStorage.freqArr ? JSON.parse(localStorage.freqArr) : [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]);
+  const [dataAvail, setDataAvail] = useState(true);
 
   useEffect(() => {
 
     (async () => {
       const result = await axios.get(`https://irris-guessing-game-server.onrender.com/v1/puzzle/${new Date(dateNow).toISOString().split('T')[0]}`)
-      const puzzle = result.data;
+      if (result.status != 200) {
+        setDataAvail(false);
+      }else {
+        const puzzle = result.data;
 
-      setPictures(puzzle.answer.pictures);
-      setAnswers(puzzle.answer.names);
+        setPictures(puzzle.answer.pictures);
+        setAnswers(puzzle.answer.names);
+      }
+     
     })();
     
   }, []);
@@ -69,8 +77,6 @@ function App() {
 
       return e;
     });
-
-    console.log(finalGuess)
 
     if (finalGuess.includes("")) {
       console.log("You haven't answered everything yet");
@@ -131,22 +137,24 @@ function App() {
 
  
     return (
-      <div className="h-100 mx-auto w-9/12 text-center text-lg relative">
-        <div className="relative top-1/2">
-          <h1 className="relative top-10 text-5xl font-bold">IRRIS Guessing Game</h1>
+      dataAvail ?
+        <div className="h-100 mx-auto w-9/12 text-center text-lg relative">
+          <div className="relative top-1/2">
+            <h1 className="relative top-10 text-5xl font-bold">IRRIS Guessing Game</h1>
 
-          <h3 className="fixed right-5 top-5 font-semibold">Tries Left: {localStorage.triesLeft}</h3>
+            <h3 className="fixed right-5 top-5 font-semibold">Tries Left: {localStorage.triesLeft}</h3>
 
-          <PhotoContainer>
-            {pictures.map((picID, i) => {
-              return <Photo key={picID} publicID={picID} num={i} guess={guess} updateGuess={setGuess} answerCheck={correctGuess} answer={answer} triesComplete={localStorage.triesComplete} freqArr={freqArr} />
-            })}
-          </PhotoContainer>
+            <PhotoContainer>
+              {pictures.map((picID, i) => {
+                return <Photo key={picID} publicID={picID} num={i} guess={guess} updateGuess={setGuess} answerCheck={correctGuess} answer={answer} triesComplete={localStorage.triesComplete} freqArr={freqArr} />
+              })}
+            </PhotoContainer>
 
-          <input className="bg-black text-white w-1/2 py-3 mt-5 rounded hover:bg-gray-500 hover:cursor-pointer" type='submit' onClick={submitGuess} />
+            <input className="bg-black text-white w-1/2 py-3 mt-5 rounded hover:bg-gray-500 hover:cursor-pointer" type='submit' onClick={submitGuess} />
+          </div>
+        
         </div>
-       
-      </div>
+        : <ComingSoonPage />
       
     )
   
---
+++ src/pages/ComingSoonPage.jsx
@@ -0,0 +1,7 @@
+export default function ComingSoonPage () {
+    return (
+        <div className="mx-auto w-9/12 text-center text-lg ">
+            <h1 className = "bold "> Demo is currently over. Please check back on a later scheduled date for full release. </h1>
+        </div>
+    )
+}
